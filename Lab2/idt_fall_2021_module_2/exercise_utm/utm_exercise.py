e442989a2c5f4c9aca4b772a2d84d56d49d767d7
nthom18 <nthom18@student.sdu.dk>
2021-09-21 14:25:16 +0200

    Added point moving North

--- Lab2/idt_fall_2021_module_2/exercise_utm/utm_exercise.py
+++ Lab2/idt_fall_2021_module_2/exercise_utm/utm_exercise.py
@@ -1,10 +1,18 @@
 """
 This code is copied from utm_test.py, and modified by Group 1.
 """
-
+#### Imports ####
 from utm import utmconv
 from math import pi, cos, acos, radians, sin, asin, sqrt
 
+#### Functions ####
+# Great circle formulae: Distance between points
+def gcf_distance(lat1, lon1, lat2, lon2):
+    distance_radians = 2 * asin(sqrt((sin((lat1 - lat2) / 2))**2 + cos(lat1) * cos(lat2) * (sin((lon1 - lon2) / 2))**2))
+    radius_km = 1.852 * (180 * 60 / pi)
+    return radius_km *  distance_radians
+
+#### Program ####
 # Geodetic reference coordinate
 lat_ref =  55.47
 lon_ref = 10.33
@@ -13,33 +21,43 @@ print ('Reference position [deg]:')
 print ('  latitude:  %.8f'  % (lat_ref))
 print ('  longitude: %.8f'  % (lon_ref))
 
-# instantiate utmconv class
+# Instantiate utmconv class
 uc = utmconv()
 
-# convert from geodetic to UTM
-(hemisphere, zone, letter, e1, n1) = uc.geodetic_to_utm (lat_ref,lon_ref)
+# Convert from geodetic to UTM
+(hemisphere, zone, letter, e_ref, n_ref) = uc.geodetic_to_utm (lat_ref,lon_ref)
 # print ('\nConverted from geodetic to UTM [m]')
-# print ('  %d %c %.5fe %.5fn' % (zone, letter, e1, n1))
+# print ('  %d %c %.5fe %.5fn' % (zone, letter, e_ref, n_ref))
 
-# now generating the second UTM coordinate
-e2 = e1 + 1000.0 
-n2 = n1 
+# MOVING EAST
+# Generating the East UTM coordinate
+e2 = e_ref + 1000.0 
+n2 = n_ref 
 
-# convert back from UTM to geodetic
+# Convert back from UTM to geodetic
 (lat_e, lon_e) = uc.utm_to_geodetic (hemisphere, zone, e2, n2)
-print ('\nSecond position 1000 meter East [deg]:')
+print ('\nPosition 1000 meter East [deg]:')
 print ('  latitude:  %.8f'  % (lat_e))
 print ('  longitude: %.8f'  % (lon_e))
 
-
-# Great circle formulae: Distance between points
-def gcf_distance(lat1, lon1, lat2, lon2):
-    distance_radians = 2 * asin(sqrt((sin((lat1 - lat2) / 2))**2 + cos(lat1) * cos(lat2) * (sin((lon1 - lon2) / 2))**2))
-    radius_km = 1.852 * (180 * 60 / pi)
-    return radius_km *  distance_radians
-
 d_e = gcf_distance(lat_ref, lon_ref, lat_e, lon_e)
-
-print ('\nGreat Circle formula:')
+print ('\nMoving East:')
 print ('  distance:     %.8f' % (d_e))
 print ('  Error in %%:  %.8f' % (100 * (d_e - 1) / 1))
+
+
+# MOVING NORTH
+# Generating the North UTM coordinate
+e3 = e_ref
+n3 = n_ref + 1000.0
+
+# Convert back from UTM to geodetic
+(lat_n, lon_n) = uc.utm_to_geodetic (hemisphere, zone, e3, n3)
+print ('\nPosition 1000 meter North [deg]:')
+print ('  latitude:  %.8f'  % (lat_n))
+print ('  longitude: %.8f'  % (lon_n))
+
+d_n = gcf_distance(lat_ref, lon_ref, lat_n, lon_n)
+print ('\nMoving North:')
+print ('  distance:     %.8f' % (d_n))
+print ('  Error in %%:  %.8f' % (100 * (d_n - 1) / 1))
