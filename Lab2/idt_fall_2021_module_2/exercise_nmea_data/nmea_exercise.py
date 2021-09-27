671227a7716b00c86d05b87ca5a2fd7c5d4c5af1
nthom18 <nthom18@student.sdu.dk>
2021-09-27 11:31:39 +0200

    Set up code to be able to handle and plot nmea_static data

--- Lab2/idt_fall_2021_module_2/exercise_nmea_data/nmea_exercise.py
+++ Lab2/idt_fall_2021_module_2/exercise_nmea_data/nmea_exercise.py
@@ -10,18 +10,18 @@ def get_row(array, index):
     time = []
     y = []
     for row in array:
-        time.append(float(row[1]))
-        y.append(float(row[index]))
+        if (row[index] != ''):
+            time.append(float(row[1]))
+            y.append(float(row[index]))
     return time, y
 
 
 #### File import ####
 
 print("Importing files...")
-nmea_eduquad = nmea_class()
-nmea_eduquad.import_file ('nmea_trimble_gnss_eduquad_flight.txt')
-# nmea_static = nmea_class()
-# nmea_static.import_file ('nmea_ublox_neo_24h_static.txt')
+nmea_data = nmea_class()
+# nmea_data.import_file ('nmea_trimble_gnss_eduquad_flight.txt')
+nmea_data.import_file ('nmea_ublox_neo_24h_static.txt')
 print("Done!")
 
 
@@ -31,22 +31,22 @@ figure, axis = plt.subplots(2, 1)
 
 # Altitude above Mean Sea Level
 # WHAT INDEX IS THIS? D:
-x, y = get_row(nmea_eduquad.data, 9)
+x, y = get_row(nmea_data.data, 9)
 axis[0].plot(x, y, 'green')
 axis[0].set_title('Altitude')
 
 # Number of satellites tracked
-x, y = get_row(nmea_eduquad.data, 7)
+x, y = get_row(nmea_data.data, 7)
 axis[1].plot(x, y, 'red')
 axis[1].set_title('Nr. of satellites')
 
-# plt.show()
+plt.show()
 
 # Map showing the drone track during the drone flight
 kml = kmlclass()
 kml.begin('drone_track.kml', 'Drone Track', 'Drone track during the drone flight', 0.7)
 kml.trksegbegin ('', '', 'red', 'absolute') 
-for row in nmea_eduquad.data:
+for row in nmea_data.data:
     # Latitude: DDMM.MMMMM to DD.DDDDD format
     degrees_lat = float(row[2][0] + row[2][1])
     minutes_lat = float(row[2][2:])
