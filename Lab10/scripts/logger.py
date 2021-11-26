372a5498449ce67d8d7994b2f35f0f161cb3a07c
Nthom18 <48620283+Nthom18@users.noreply.github.com>
2021-11-26 09:49:47 +0100

    Added scripts from mavlink_lora

---
+++ Lab10/scripts/logger.py
@@ -0,0 +1,29 @@
+"""
+Logging passed information to csv files format.
+
+Author: Nicoline Louise Thomsen
+"""
+
+import csv
+import datetime
+import numpy as np
+
+class Logger():
+
+    def __init__(self):
+        
+        time = str(datetime.datetime.now())[11:19]
+        dateid = time.replace(':', '')
+
+        file_name = 'logs/data_' + dateid +'.csv'
+        log = open(file_name, 'w+', newline='')  # w+ mode truncates (clears) the file (new file for every test)   
+        
+        self.logger = csv.writer(log, dialect = 'excel')
+
+
+    def log_to_file(self, *data):
+
+        row = [data]
+        # row.extend(data)
+
+        self.logger.writerow(row)
