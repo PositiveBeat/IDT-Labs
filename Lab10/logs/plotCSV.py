598220e4d5655af3dca04bdacd539095e2b1c815
Nthom18 <48620283+Nthom18@users.noreply.github.com>
2021-11-26 09:50:41 +0100

    Added logging files

---
+++ Lab10/logs/plotCSV.py
@@ -0,0 +1,49 @@
+'''
+Plotting csv files for case d).
+
+Author: Nicoline Louise Thomsen
+'''
+
+import csv
+from exportkml import kmlclass
+import matplotlib.pyplot as plt
+
+lat = []
+lon = []
+
+def plotCSV_d(filename):
+    
+    with open(filename + '.csv','r') as csvfileQuick:
+        plots = csv.reader(csvfileQuick, delimiter=',')
+        for row in plots:
+            lat.append(float(row[0]))
+            lon.append(float(row[1]))
+
+    # plt.plot(lat, lon, label = 'Position')
+    # plt.xlabel('Lattitude')
+    # plt.ylabel('Longitude')
+    # plt.title('GNSS Position')
+    # plt.legend()
+    
+    # plt.show()
+    
+
+
+if __name__ == '__main__':
+    plotCSV_d('data_085254')
+    
+    # Map showing the drone track during the drone flight
+    kml = kmlclass()
+    kml.begin('drone_track.kml', 'Drone Track', 'Drone track during the drone flight', 0.7)
+    kml.trksegbegin ('', '', 'red', 'absolute') 
+    
+    for i in range(len(lat)):
+
+        la = lat[i]
+        lo = lon[i]
+
+        # Add to file
+        kml.pt(float(la), float(lo), 40)
+        
+    kml.trksegend()
+    kml.end()
