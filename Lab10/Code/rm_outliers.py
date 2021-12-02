7b3fa6711c13d678824b80a930ae32266c43c0eb
nthom18 <nthom18@student.sdu.dk>
2021-12-02 18:46:27 +0100

    update

--- Lab10/Code/rm_outliers.py
+++ Lab10/Code/rm_outliers.py
@@ -11,9 +11,9 @@ class outliers_begone:
     def purge(self, utm):
         
         accepted = []
-        t_prev = utm[0]
-        n_prev = utm[1]
-        e_prev = utm[2]
+        t_prev = utm[0][0]
+        n_prev = utm[1][0]
+        e_prev = utm[2][0]
 
         for i, row in enumerate(utm):
             time = row[0]
@@ -24,12 +24,10 @@ class outliers_begone:
                 accepted.append([time, northing, easting])
             
             else:
-                dist = sqrt( (northing - n_prev)**2 + (easting - e_prev)**2 )
+                dist = sqrt((northing - n_prev)**2 + (easting - e_prev)**2)
                 time_diff = time - t_prev
-                
-                print(time_diff)
             
-                margin = 0.5 + time_diff*0.1
+                margin = 0.5 + time_diff*0.5
                 if (dist < (1.4 * time_diff + margin)):   # 1.4 meters pr. second + margin
                     accepted.append([time, northing, easting])
                     
